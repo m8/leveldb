d66ce8454df99c304ccad43c8c2ef9cb79046652
m8 <umusasadik@gmail.com>
2022-10-13 14:22:19 +0200

    Makefile change concord^s path

--- concord.mk
+++ concord.mk
@@ -11,25 +11,30 @@ OPT = opt-9
 CXX := clang++-9 -stdlib=libc++ -fPIC
 CXX_FLAGS := -emit-llvm -g -S -O3 -I. -I./include -pthread -DOS_LINUX -DLEVELDB_PLATFORM_POSIX -DSNAPPY  -c 
 
-CONCORD_PASS=../src/pass/build/src/libConcordPass.so
-CONCORD_LIB=../src/lib/concord.a
-CONCORD_DIR=../src/lib/
+ROOT_DIR:=$(shell dirname $(realpath $(firstword $(MAKEFILE_LIST))))
+
+CONCORD_MAIN=$(ROOT_DIR)/../../../src
+
+CONCORD_PASS=$(CONCORD_MAIN)/pass/build/src/libConcordPass.so
+CONCORD_LIB=$(CONCORD_MAIN)/lib/concord.a
+CONCORD_DIR=$(CONCORD_MAIN)/lib/
 INC_DIR=-I$(CONCORD_DIR)
 
 PASS_CONFIG = -load $(CONCORD_PASS) -yield
 OPT_CONFIG = -postdomtree -mem2reg -indvars -loop-simplify -branch-prob -scalar-evolution
 
 all: clean build_db build_util build_table 
-	$(MAKE) concord_pass
+	$(MAKE) -f concord.mk concord_pass
 
 concord_pass:
 	llvm-link-9 -o libleveldb.bc $(BUILD_DIR)/*.bc
 	llvm-dis-9 libleveldb.bc -o leveldb.ll
 	$(OPT) -S $(OPT_CONFIG) < leveldb.ll > leveldb.opt.ll
 	$(OPT) -S -load $(CONCORD_PASS) -yield < leveldb.opt.ll > concord_leveldb.opt.ll
-	$(CXX) -std=c++11 -c concord_leveldb.opt.ll -O3 -o concord_libleveldb.a -lpthread -lci
+	$(CXX) -std=c++11 -c -O3 concord_leveldb.opt.ll -o concord_libleveldb.a
 
 build_db:
+	@mkdir -p $(BUILD_DIR)
 	for file in $(DB_FOLDER); do \
 		$(CXX) $(CXX_FLAGS) db/$$file.cc -o $(BUILD_DIR)/$$file.bc; \
 		clang++-9 -std=c++11 -c $(BUILD_DIR)/$$file.bc -o $(BUILD_DIR)/$$file.o; \
@@ -48,4 +53,4 @@ build_table:
 	done
 
 clean:
-	$(RM) $(BUILD_DIR)/*.o $(BUILD_DIR)/*.bc  $(BUILD_DIR)/*.o *.ll *.opt.ll *.bc
+	$(RM) $(BUILD_DIR)/*.o $(BUILD_DIR)/*.bc  $(BUILD_DIR)/*.o *.ll *.opt.ll *.bc concord_libleveldb.a
