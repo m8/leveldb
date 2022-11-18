efdc4cbf4ac8c836d4d85345547d16884d84e5f3
m8 <umusasadik@gmail.com>
2022-11-18 23:53:06 +0100

    update makefile

--- concord.mk
+++ concord.mk
@@ -4,11 +4,9 @@ TABLE_FOLDER := block_builder block filter_block format iterator merger table_bu
 UTIL_FOLDER := arena bloom cache coding comparator crc32c env env_posix filter_policy hash histogram logging options status
 BUILD_DIR := concord_build
 
-LLVM_CONFIG = llvm-config-9
-LLVM_LINK = llvm-link-9
-OPT = opt-9
+OPT = opt-10
 
-CXX := clang++-9 -fPIC
+CXX := clang++-10
 CXX_FLAGS := -emit-llvm -g -S -O3 -I. -I./include -pthread -DOS_LINUX -DLEVELDB_PLATFORM_POSIX -DSNAPPY  -c 
 
 ROOT_DIR:=$(shell dirname $(realpath $(firstword $(MAKEFILE_LIST))))
@@ -16,7 +14,7 @@ ROOT_DIR:=$(shell dirname $(realpath $(firstword $(MAKEFILE_LIST))))
 CONCORD_MAIN=$(ROOT_DIR)/../../../src
 
 CONCORD_PASS=$(CONCORD_MAIN)/pass/build/src/libConcordPass.so
-CONCORD_LIB=$(CONCORD_MAIN)/lib/concord.a
+RDTSC_PASS=$(CONCORD_MAIN)/rdtsc-pass/build/src/libConcordPass.so
 CONCORD_DIR=$(CONCORD_MAIN)/lib/
 INC_DIR=-I$(CONCORD_DIR)
 
@@ -27,30 +25,31 @@ all: clean build_db build_util build_table
 	$(MAKE) -f concord.mk concord_pass
 
 concord_pass:
-	llvm-link-9 -o libleveldb.bc $(BUILD_DIR)/*.bc
-	llvm-dis-9 libleveldb.bc -o leveldb.ll
+	llvm-link-10 -o libleveldb.bc $(BUILD_DIR)/*.bc
+	llvm-dis-10 libleveldb.bc -o leveldb.ll
 	$(OPT) -S $(OPT_CONFIG) < leveldb.ll > leveldb.opt.ll
+	
 	$(OPT) -S -load $(CONCORD_PASS) -yield < leveldb.opt.ll > concord_leveldb.opt.ll
-	$(CXX) -c -O3 concord_leveldb.opt.ll -o concord_libleveldb.a $(CONCORD_LIB) 
-	$(CXX) -c -O3 leveldb.opt.ll -o concord_libleveldb_clear.a $(CONCORD_LIB) 
+	$(OPT) -S -load $(RDTSC_PASS) -yield < leveldb.opt.ll > concord_leveldb.rdtsc.opt.ll
+	
+	$(CXX) -c -O3 concord_leveldb.opt.ll -o concord_libleveldb.a
+	$(CXX) -c -O3 concord_leveldb.rdtsc.opt.ll -o concord_libleveldb_rdtsc.a
+	$(CXX) -c -O3 leveldb.ll -o concord_libleveldb_clear.a
 
 build_db:
 	@mkdir -p $(BUILD_DIR)
 	for file in $(DB_FOLDER); do \
 		$(CXX) $(CXX_FLAGS) db/$$file.cc -o $(BUILD_DIR)/$$file.bc $(INC_DIR); \
-		clang++-9 -c $(BUILD_DIR)/$$file.bc -o $(BUILD_DIR)/$$file.o $(INC_DIR); \
 	done
 
 build_util:
 	for file in $(UTIL_FOLDER); do \
 		$(CXX) $(CXX_FLAGS) util/$$file.cc -o $(BUILD_DIR)/$$file.bc $(INC_DIR); \
-		clang++-9 -c $(BUILD_DIR)/$$file.bc -o $(BUILD_DIR)/$$file.o $(INC_DIR); \
 	done
 
 build_table:
 	for file in $(TABLE_FOLDER); do \
 		$(CXX) $(CXX_FLAGS) table/$$file.cc -o $(BUILD_DIR)/$$file.bc $(INC_DIR); \
-		clang++-9 -c $(BUILD_DIR)/$$file.bc -o $(BUILD_DIR)/$$file.o $(INC_DIR); \
 	done
 
 clean:
