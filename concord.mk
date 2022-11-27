e5208a05e446b18c35b0cd42d9bf79398dc96119
rishabh246 <rishabh246@gmail.com>
2022-11-27 23:45:57 +0100

    Compile with -fPIC to link into a shared library

--- concord.mk
+++ concord.mk
@@ -4,10 +4,12 @@ TABLE_FOLDER := block_builder block filter_block format iterator merger table_bu
 UTIL_FOLDER := arena bloom cache coding comparator crc32c env env_posix filter_policy hash histogram logging options status
 BUILD_DIR := concord_build
 
-OPT = opt-10
+LLVM_VERSION=9
 
-CXX := clang++-10
-CXX_FLAGS := -emit-llvm -g -S -O3 -I. -I./include -pthread -DOS_LINUX -DLEVELDB_PLATFORM_POSIX -DSNAPPY  -c 
+OPT = opt-$(LLVM_VERSION)
+
+CXX := clang++-$(LLVM_VERSION)
+CXX_FLAGS := -emit-llvm -g -S -O3 -I. -I./include -pthread -DOS_LINUX -DLEVELDB_PLATFORM_POSIX -DSNAPPY  -c -fPIC
 
 ROOT_DIR:=$(shell dirname $(realpath $(firstword $(MAKEFILE_LIST))))
 
@@ -25,16 +27,16 @@ all: clean build_db build_util build_table
 	$(MAKE) -f concord.mk concord_pass
 
 concord_pass:
-	llvm-link-10 -o libleveldb.bc $(BUILD_DIR)/*.bc
-	llvm-dis-10 libleveldb.bc -o leveldb.ll
+	llvm-link-$(LLVM_VERSION) -o libleveldb.bc $(BUILD_DIR)/*.bc
+	llvm-dis-$(LLVM_VERSION) libleveldb.bc -o leveldb.ll
 	$(OPT) -S $(OPT_CONFIG) < leveldb.ll > leveldb.opt.ll
 	
 	$(OPT) -S -load $(CONCORD_PASS) -yield < leveldb.opt.ll > concord_leveldb.opt.ll
 	$(OPT) -S -load $(RDTSC_PASS) -yield < leveldb.opt.ll > concord_leveldb.rdtsc.opt.ll
 	
-	$(CXX) -c -O3 concord_leveldb.opt.ll -o concord_libleveldb.a
-	$(CXX) -c -O3 concord_leveldb.rdtsc.opt.ll -o concord_libleveldb_rdtsc.a
-	$(CXX) -c -O3 leveldb.ll -o concord_libleveldb_clear.a
+	$(CXX) -c -O3 -fPIC concord_leveldb.opt.ll -o concord_libleveldb.a
+	$(CXX) -c -O3 -fPIC concord_leveldb.rdtsc.opt.ll -o concord_libleveldb_rdtsc.a
+	$(CXX) -c -O3 -fPIC leveldb.ll -o concord_libleveldb_clear.a
 
 build_db:
 	@mkdir -p $(BUILD_DIR)
